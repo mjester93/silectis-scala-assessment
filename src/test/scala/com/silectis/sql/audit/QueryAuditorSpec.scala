a8b3de5eaf3fc65b18bd9bc946eb8350a1bd24bf
Michael Jester <m.jester.93@gmail.com>
2025-01-01 21:20:54 -0500

    feat(audit): Implement QueryAuditor audit feature with recusive parsing.
    Add logic to parse columns and tables recusively. Includes the handling of subqueries, column aliases, and table references.
    
    Add unit tests covering basic queries, subqueries, nested subqueries, and edge cases such as:
        - queries without tables
        - queries with non-column expressions

--- src/test/scala/com/silectis/sql/audit/QueryAuditorSpec.scala
+++ src/test/scala/com/silectis/sql/audit/QueryAuditorSpec.scala
@@ -1,10 +1,13 @@
 package com.silectis.sql.audit
 
 import com.silectis.sql.SqlException
+import com.silectis.sql.TableExpression
+import com.silectis.sql.TableReference
 import com.silectis.test.UnitSpec
+import scala.util.Success
 
-/**
-  * @author jlouns
+/** @author
+  *   jlouns
   */
 class QueryAuditorSpec extends UnitSpec {
   val auditor = new QueryAuditor
@@ -12,5 +15,80 @@ class QueryAuditorSpec extends UnitSpec {
   it should "return failure for an invalid SQL query" in {
     a[SqlException] should be thrownBy
       auditor.audit("not valid sql").get
+  }
+
+  it should "return the correct query from a table" in {
+    val parsed = auditor.audit("select col1, col2 from table1")
+    parsed shouldBe Success(
+      QueryAuditResult(
+        Some(TableReference(None, "table1")),
+        List("col1", "col2")
+      )
+    )
+  }
+
+  it should "return the correct query from a table with a schema" in {
+    val parsed = auditor.audit("SELECT col1 FROM schema1.table1")
+    parsed shouldBe Success(
+      QueryAuditResult(
+        Some(TableReference(Some("schema1"), "table1")),
+        List("col1")
+      )
+    )
+  }
+
+  it should "return the correct query with no table and no columns" in {
+    val parsed = auditor.audit("select 1 as one, 'two'")
+    parsed shouldBe Success(
+      QueryAuditResult(
+        None,
+        List()
+      )
+    )
+  }
+
+  it should "return the correct query with a column expression non-nested subquery" in {
+    val parsed = auditor.audit("SELECT 1, upper('test') AS name FROM table1")
+    parsed shouldBe Success(
+      QueryAuditResult(Some(TableReference(None, "table1")), List())
+    )
+  }
+
+  it should "return the correct query with a subquery and aliases" in {
+    val parsed = auditor.audit("""
+    select a, b, x, y from (
+      select
+        col1 as a,
+        2 as b,
+        x,
+        upper('test') as y
+      from table2
+    ) as s
+    """)
+    parsed shouldBe Success(
+      QueryAuditResult(
+        Some(TableReference(None, "table2")),
+        List("col1", "x")
+      )
+    )
+  }
+
+  it should "return the correct query with a nested subquery" in {
+    val parsed = auditor.audit("""
+    SELECT x, y FROM (
+      SELECT a AS x, b AS y FROM (
+        SELECT
+          col1 AS a,
+          col2 AS b
+        FROM table3
+      ) AS inner
+    ) AS outer
+    """)
+    parsed shouldBe Success(
+      QueryAuditResult(
+        Some(TableReference(None, "table3")),
+        List("col1", "col2")
+      )
+    )
   }
 }
